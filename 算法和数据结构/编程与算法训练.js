5fae274fcaa049a893b562e3467939853d8412de
zhaoyang007 <1559759403@qq.com>
2021-02-18 21:57:03 +0800

    算法总结

---
+++ 算法和数据结构/编程与算法训练.js
@@ -0,0 +1,32 @@
+/**
+ * 557.反转字符串中的单词 III
+ * 给定一个字符串，你需要反转字符串中每个单词的字符顺序，同时仍保留空格和单词的初始顺序。
+ * 输入："Let's take LeetCode contest"
+ * 输出："s'teL ekat edoCteeL tsetnoc"
+ */
+function reverseWord1(str) {
+  let result = str.split(' ').map(item => {
+    return item.split('').reverse().join('');
+  }).join(' ');
+  return result;
+}
+function reverseWord2(str) {
+  let result = str.split(/\s/g).map(item => {
+    return item.split('').reverse().join('');
+  }).join(' ');
+  return result;
+}
+function reverseWord3(str) {
+  let result = str.match(/[\w']+/g).map(item => {
+    return item.split('').reverse().join('');
+  }).join(' ');
+  return result;
+}
+console.log(reverseWord3("Let's take LeetCode contest"))
+
+/**
+ * 
+ * 
+ * 
+ */
+
