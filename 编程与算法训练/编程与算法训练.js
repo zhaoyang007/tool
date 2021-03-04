19ec10822b552d37822bf0e9a92c35477ffdb9b1
zhaoyang007 <1559759403@qq.com>
2021-03-04 14:30:20 +0800

    冒泡排序

--- 编程与算法训练/编程与算法训练.js
+++ 编程与算法训练/编程与算法训练.js
@@ -411,6 +411,20 @@ function bubbleSort(arr) {
 }
 // console.log(bubbleSort([1,9,3,6,5,4]));
 
+function bubbleSort2(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      let tmp = arr[j];
+      if (arr[j] > arr[j + 1]) {
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  } 
+  return arr;
+}
+console.log(bubbleSort2([1,9,3,20,6,5,4,10]));
+
 /**
  * 2.选择排序
  */
