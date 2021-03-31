a3b2cbe6dc02dc696b6e5efc4d4b59542690a1de
zhaoyang007 <1559759403@qq.com>
2021-03-31 20:54:05 +0800

    数据结构: 堆栈

--- 编程与算法训练/编程与算法训练.js
+++ 编程与算法训练/编程与算法训练.js
@@ -1,3 +1,4 @@
+/* 一、基础算法 */
 // 字符串
 /**
  * 1.反转字符串中的单词 III(557)
@@ -796,5 +797,60 @@ function getSubStr(str, words) {
     return str.indexOf(item);
   }).filter(item => item !== -1);
 }
-console.log(getSubStr('barfoothefoobarman', ["foo","bar"]))
+// console.log(getSubStr('barfoothefoobarman', ["foo","bar"]))
+
+/* 二、数据结构 */
+// 栈
+/**
+ * 1.棒球比赛(682)
+ * 你现在是一场采用特殊赛制棒球比赛的记录员。这场比赛由若干回合组成，过去几回合的得分可能会
+ * 影响以后几回合的得分。比赛开始时，记录是空白的。你会得到一个记录操作的字符串列表 ops，
+ * 其中 ops[i] 是你需要记录的第 i 项操作，ops 遵循下述规则：
+ * 整数 x - 表示本回合新获得分数 x
+ * "+" - 表示本回合新获得的得分是前两次得分的总和。题目数据保证记录此操作时前面总是存在两个有效的分数。
+ * "D" - 表示本回合新获得的得分是前一次得分的两倍。题目数据保证记录此操作时前面总是存在一个有效的分数。
+ * "C" - 表示前一次得分无效，将其从记录中移除。题目数据保证记录此操作时前面总是存在一个有效的分数。
+ * 请你返回记录中所有得分的总和。
+ * 
+ * 输入：ops = ["5","2","C","D","+"]
+ * 输出：30
+ * 解释：
+ * "5" - 记录加 5 ，记录现在是 [5]
+ * "2" - 记录加 2 ，记录现在是 [5, 2]
+ * "C" - 使前一次得分的记录无效并将其移除，记录现在是 [5].
+ * "D" - 记录加 2 * 5 = 10 ，记录现在是 [5, 10].
+ * "+" - 记录加 5 + 10 = 15 ，记录现在是 [5, 10, 15].
+ * 所有得分的总和 5 + 10 + 15 = 30
+ */
+function recordScore(arr) {
+  let res = 0;
+  let score = [];
+  arr.forEach(item => {
+    switch (item) {
+      case 'C': 
+        if (score.length) {
+          score.pop();
+        }
+        break
+      case 'D': 
+        if (score.length > 0) {
+          let pre = score.pop();
+          score.push(pre, pre * 2);
+        }
+        break
+      case '+': 
+        if (score.length > 1) {
+          let pre1 = score.pop();
+          let pre2 = score.pop();
+          score.push(pre2, pre1, Number(pre1) + Number(pre2));
+        }
+        break
+      default: 
+        score.push(item);
+    }
+  });
+  score.forEach(item => res += Number(item));
+  return res;
+}
+console.log(recordScore(["5","2","C","D","+"]))
 
