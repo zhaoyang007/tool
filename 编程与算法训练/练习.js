7ff28d64c3985c676eb63839d5a727e3f40e92dd
zhaoyang007 <1559759403@qq.com>
2021-06-03 18:59:12 +0800

    算法练习

--- 编程与算法训练/练习.js
+++ 编程与算法训练/练习.js
@@ -1,1 +1,241 @@
+// 移动零：1.遇零删除，记录数量，最后添加零 2. 利用新的数据存储非零和零。3. 双指针交换
+// 1
+// function moveZeroes(nums) {
+//   let count = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === 0) {
+//       nums.splice(i, 1);
+//       count++;
+//       i--;
+//     }
+//   }
+//   for (let i = 0; i < count; i++) {
+//     nums.push(0);
+//   }
+//   return nums;
+// }
+
+// 2
+// function moveZeroes(nums) {
+//   let res1 = [];
+//   let res2 = [];
+//   nums.forEach(item => {
+//     if (item === 0) {
+//       res1.push(item);
+//     } else {
+//       res2.push(item);
+//     }
+//   });
+//   return res2.concat(res1);
+// }
+
+// function moveZeroes(nums) {
+//   let j = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0) {
+//       let c = nums[j];
+//       nums[j] = nums[i];
+//       nums[i] = c;
+//       j++;
+//     }
+//   }
+//   return nums;
+// }
+
+// function moveZeroes(nums) {
+//   let j = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0) {
+//       let c = nums[j];
+//       nums[j] = nums[i];
+//       if (c === 0) {
+//         nums[i] = 0;
+//       }
+//       j++;
+//     }
+//   }
+//   return nums;
+// }
+
+// 盛水最多的容器 1.双循环 2.左右夹逼
+// function maxArea(arr) {
+//   let max = 0;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       let area = Math.min(arr[i], arr[j]) * (j - i);
+//       max = Math.max(max, area);
+//     }
+//   }
+//   return max;
+// }
+
+// function maxArea(arr) {
+//   let max = 0;
+//   for (let i = 0, j = arr.length - 1; i < j; ) {
+//     let minHeight = arr[i] < arr[j] ? arr[i++] : arr[j--];
+//     let area = (j - i + 1) * minHeight;
+//     max = Math.max(max, area);
+//   }
+//   return max;
+// }
+
+// 爬楼梯 1.暴力递归 2.记忆化递归 3.动态规划
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   return climbStairs(n - 1) + climbStairs(n - 2);
+// }
+
+// let mapData = new Map();
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   if (mapData.get(n)) {
+//     return mapData.get(n);
+//   }
+//   let res = climbStairs(n - 1) + climbStairs(n - 2);
+//   mapData.set(n, res);
+//   return res;
+// }
+
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   let arr = [];
+//   arr[1] = 1;
+//   arr[2] = 2;
+//   for (let i = 3; i <= n; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//   } 
+//   return arr[n];
+// }
+
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   let a = 1, b = 2, r = 0;
+//   for (let i = 0; i < n - 2; i++) {
+//     r = a + b;
+//     a = b;
+//     b = r;
+//   }
+//   return r;
+// }
+
+// 移动零：1.遇零删除，记录数量，最后添加零 2. 利用新的数据存储非零和零。3. 双指针交换
+// 1
+// function moveZeroes(nums) {
+//   let count = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === 0) {
+//       nums.splice(i, 1);
+//       count++;
+//       i--;
+//     }
+//   }
+//   for (let i = 0; i < count; i++) {
+//     nums.push(0);
+//   }
+//   return nums;
+// }
+
+// 2
+// function moveZeroes(nums) {
+//   let res1 = [];
+//   let res2 = [];
+//   nums.forEach(item => {
+//     if (item === 0) {
+//       res1.push(item);
+//     } else {
+//       res2.push(item);
+//     }
+//   });
+//   return res2.concat(res1);
+// }
+
+// function moveZeroes(nums) {
+//   let j = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0) {
+//       let c = nums[j];
+//       nums[j] = nums[i];
+//       nums[i] = c;
+//       j++;
+//     }
+//   }
+//   return nums;
+// }
+
+// function moveZeroes(nums) {
+//   let j = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0) {
+//       let c = nums[j];
+//       nums[j] = nums[i];
+//       if (c === 0) {
+//         nums[i] = 0;
+//       }
+//       j++;
+//     }
+//   }
+//   return nums;
+// }
+
+// 盛水最多的容器 1.双循环 2.左右夹逼
+// function maxArea(arr) {
+//   let max = 0;
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       let area = Math.min(arr[i], arr[j]) * (j - i);
+//       max = Math.max(max, area);
+//     }
+//   }
+//   return max;
+// }
+
+// function maxArea(arr) {
+//   let max = 0;
+//   for (let i = 0, j = arr.length - 1; i < j; ) {
+//     let minHeight = arr[i] < arr[j] ? arr[i++] : arr[j--];
+//     let area = (j - i + 1) * minHeight;
+//     max = Math.max(max, area);
+//   }
+//   return max;
+// }
+
+// 爬楼梯 1.暴力递归 2.记忆化递归 3.动态规划
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   return climbStairs(n - 1) + climbStairs(n - 2);
+// }
+
+// let mapData = new Map();
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   if (mapData.get(n)) {
+//     return mapData.get(n);
+//   }
+//   let res = climbStairs(n - 1) + climbStairs(n - 2);
+//   mapData.set(n, res);
+//   return res;
+// }
+
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   let arr = [];
+//   arr[1] = 1;
+//   arr[2] = 2;
+//   for (let i = 3; i <= n; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//   } 
+//   return arr[n];
+// }
+
+// function climbStairs(n) {
+//   if (n <= 2) return n;
+//   let a = 1, b = 2, r = 0;
+//   for (let i = 0; i < n - 2; i++) {
+//     r = a + b;
+//     a = b;
+//     b = r;
+//   }
+//   return r;
+// }
+
 
