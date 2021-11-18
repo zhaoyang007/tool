c221a155ea534ed0541dd78c2335a6f2e84789c0
zhaoyang007 <1559759403@qq.com>
2021-11-18 21:25:37 +0800

    面试练习

--- 大前端知识体系/lianxi.js
+++ 大前端知识体系/lianxi.js
@@ -1795,3 +1795,72 @@ function isObject(obj) {
 // sleep(1000).then(res => {
 //     console.log(111);
 // })
+// function add(...args) {
+//     let allArgs = [...args];
+//     function fn(...newArgs) {
+//         allArgs = [...allArgs, ...newArgs];
+//         return fn;
+//     }
+//     fn.toString = function () {
+//         if (!allArgs.length) {
+//         return;
+//         }
+//         return allArgs.reduce((sum, cur) => sum + cur);
+//     };
+//     return fn;
+// }
+// console.log(add(1)(2)(3, 4).toString());
+// var add = function (m) {
+//     var temp = function (n) {
+//       return add(m + n);
+//     }
+//     temp.toString = function () {
+//       return m;
+//     }
+//     return temp;
+// };
+// console.log(add(3)(4)(5));
+// function add (...args) {
+//     //求和
+//     return args.reduce((a, b) => a + b)
+// }
+// function currying(fn) {
+//     let args = []
+//     return function temp(...newArgs) {
+//         if (newArgs.length) {
+//             args = [
+//                 ...args,
+//                 ...newArgs
+//             ]
+//             return temp
+//         } else {
+//             let val = fn.apply(this, args)
+//             args = [] //保证再次调用时清空
+//             return val
+//         }
+//     }
+// }
+// let addCurry = currying(add)
+// console.log(addCurry(1)(2)(3)(4, 5))  //15
+// console.log(addCurry(1)(2)(3, 4, 5)())  //15
+// console.log(addCurry(1)(2, 3, 4, 5)()) 
+function add() { 
+    var tmpSlice = [].slice,
+        params = tmpSlice.call(arguments);
+    function currying() {
+        var arr = tmpSlice.call(arguments);
+        params = params.concat(arr);
+        return currying;
+    }
+    currying.toString = function () {
+        var result = 0;
+        params.forEach(value => {
+            result += value;
+        });
+        return result;
+    }
+    return currying;
+ }
+
+ console.log(add(1)(2)(3)) // 6
+ console.log(add(1,2,3))
