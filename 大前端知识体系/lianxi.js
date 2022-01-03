0c2955d2e455d009019ba61150d43fb0b34db7e3
zhaoyang007 <1559759403@qq.com>
2022-01-03 22:25:34 +0800

    面试练习

--- 大前端知识体系/lianxi.js
+++ 大前端知识体系/lianxi.js
@@ -7789,3 +7789,203 @@ function isObject(obj) {
 // function LazyMan(name) {
 //     return new LazyManClass(name);
 // }
+// 1
+// MyPromise.js
+// class Mypromise {
+//     constructor(fn) {
+//       // 表示状态
+//       this.state = "pending";
+//       // 表示then注册的成功函数
+//       this.successFun = [];
+//       // 表示then注册的失败函数
+//       this.failFun = [];
+  
+//       let resolve = (val) => {
+//         // 保持状态改变不可变（resolve和reject只准触发一种）
+//         if (this.state !== "pending") return;
+  
+//         // 成功触发时机  改变状态 同时执行在then注册的回调事件
+//         this.state = "success";
+//         // 为了保证then事件先注册（主要是考虑在promise里面写同步代码） promise规范 这里为模拟异步
+//         setTimeout(() => {
+//           // 执行当前事件里面所有的注册函数
+//           this.successFun.forEach((item) => item.call(this, val));
+//         });
+//       };
+  
+//       let reject = (err) => {
+//         if (this.state !== "pending") return;
+//         // 失败触发时机  改变状态 同时执行在then注册的回调事件
+//         this.state = "fail";
+//         // 为了保证then事件先注册（主要是考虑在promise里面写同步代码） promise规范 这里模拟异步
+//         setTimeout(() => {
+//           this.failFun.forEach((item) => item.call(this, err));
+//         });
+//       };
+//       // 调用函数
+//       try {
+//         fn(resolve, reject);
+//       } catch (error) {
+//         reject(error);
+//       }
+//     }
+  
+//     // 实例方法 then
+  
+//     then(resolveCallback, rejectCallback) {
+//       // 判断回调是否是函数
+//       resolveCallback = typeof resolveCallback === "function" ? resolveCallback : (v) => v;
+//       rejectCallback = typeof rejectCallback !== "function" ? (err) => { throw err; } : rejectCallback;
+//       // 为了保持链式调用  继续返回promise
+//       return new Mypromise((resolve, reject) => {
+//         // 将回调注册到successFun事件集合里面去
+//         this.successFun.push((val) => {
+//           try {
+//             //    执行回调函数
+//             let x = resolveCallback(val);
+//             //（最难的一点）
+//             // 如果回调函数结果是普通值 那么就resolve出去给下一个then链式调用  如果是一个promise对象（代表又是一个异步） 那么调用x的then方法 将resolve和reject传进去 等到x内部的异步 执行完毕的时候（状态完成）就会自动执行传入的resolve 这样就控制了链式调用的顺序
+//             x instanceof Mypromise ? x.then(resolve, reject) : resolve(x);
+//           } catch (error) {
+//             reject(error);
+//           }
+//         });
+  
+//         this.failFun.push((val) => {
+//           try {
+//             //    执行回调函数
+//             let x = rejectCallback(val);
+//             x instanceof Mypromise ? x.then(resolve, reject) : reject(x);
+//           } catch (error) {
+//             reject(error);
+//           }
+//         });
+//       });
+//     }
+// }
+// // 2
+// // 先定义三个常量表示状态
+// const 'pending' = 'pending';
+// const 'fulfilled' = 'fulfilled';
+// const 'rejected' = 'rejected';
+// // 新建 MyPromise 类
+// class MyPromise {
+//     constructor(executor) {
+//         this.status = 'pending';
+//         this.onFulfilledCallbacks = [];
+//         this.onRejectedCallbacks = [];
+//         // 更改成功后的状态
+//         const resolve = value => {
+//             if (this.status === 'pending') {
+//                 this.status = 'fulfilled';
+//                 this.onFulfilledCallbacks.forEach(fn => fn(value));
+//             }
+//         }
+//         // 更改失败后的状态
+//         const reject = err => {
+//             if (this.status === 'pending') {
+//                 this.status = 'rejected';
+//                 this.onRejectedCallbacks.forEach(fn => fn(err));
+//             }
+//         }
+//         try {
+//             executor(resolve, reject)
+//         } catch (error) {
+//             this.reject(error)
+//         }
+//     }
+//     then(onFulfilled, onRejected) {
+//         const onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v;
+//         const onRejected = typeof onRejected === 'function' ? onRejected : err => {throw err};
+//         // 为了链式调用，这里直接返回一个 MyPromise
+//         return new MyPromise((resolve, reject) => {
+//             const fulfilledMicrotask = value =>  {
+//                 queueMicrotask(() => {
+//                     try {
+//                         const x = onFulfilled(value);
+//                         x instanceof Mypromise ? x.then(resolve, reject) : resolve(x);
+//                     } catch (error) {
+//                         reject(error)
+//                     } 
+//                 })  
+//             }
+//             const rejectedMicrotask = value => { 
+//                 queueMicrotask(() => {
+//                     try {
+//                         const x = onRejected(value);
+//                         x instanceof Mypromise ? x.then(resolve, reject) : reject(x);
+//                     } catch (error) {
+//                         reject(error)
+//                     } 
+//                 }) 
+//             }
+//             this.onFulfilledCallbacks.push(fulfilledMicrotask);
+//             this.onRejectedCallbacks.push(rejectedMicrotask);
+//         }) 
+//     }
+//     static resolve(value) {
+//         if (value instanceof MyPromise) {
+//             return value;
+//         }
+//         return new MyPromise(resolve =>  {
+//             resolve(value);
+//         });
+//     }
+//     static reject(err) {
+//         return new MyPromise((resolve, reject) => {
+//             reject(err);
+//         });
+//     }
+// }
+// class Promise {
+//     constructor(fn) {
+//         this.status = 'padding';
+//         this.onFulfilledCallbacks = [];
+//         this.onRejectedCallbacks = [];
+//         const resolve = value => {
+//             if (this.status === 'padding') {
+//                 this.status = 'fulfiled';
+//                 this.onFulfilledCallbacks.forEach(fn => fn(vlaue));
+//             }
+//         }
+//         const reject = err => {
+//             if (this.status === 'padding') {
+//                 this.status = 'rejected';
+//                 this.onRejectedCallbacks.forEach(fn => fn(err));
+//             }
+//         }
+//         try {
+//             fn(resolve, reject);
+//         } catch (err) {
+//             reject(err);
+//         }
+//     }
+//     then(onFulfilled, onRejected) {
+//         const onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v;
+//         const onRejected = typeof onRejected === 'function' ? onRejected : err => { throw err};
+//         return new Promise((resolve, reject) => {
+//             const fulfilledMicrotask = value => {
+//                 queueMicrotask(() => {
+//                     try {
+//                         const res = onFulfilled(value);
+//                         res instanceof Promise ? res.then(resolve, reject) : resolve(res);
+//                     } catch(err) {
+//                         reject(err);
+//                     }
+//                 });
+//             }
+//             const rejectedMicrotask = value => {
+//                 queueMicrotask(() => {
+//                     try {
+//                         const res = onRejected(value);
+//                         res instanceof Promise ? res.then(resolve, reject) : reject(res);
+//                     } catch(err) {
+//                         reject(err);
+//                     }
+//                 });
+//             }
+//             this.onFulfilledCallbacks.push(fulfilledMicrotask);
+//             this.onRejectedCallbacks.puahs(rejectedMicrotask);
+//         });
+//     }
+// }
